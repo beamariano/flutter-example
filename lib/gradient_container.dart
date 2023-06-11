889562d4c5d3286ba756cd66838fe0379eae1629
Bea M <64287361+beamariano@users.noreply.github.com>
2023-06-11 13:16:29 +0800

    separate files

---
+++ lib/gradient_container.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 175, 0, 58),
+            Colors.black26,
+          ],
+          begin: Alignment.bottomCenter,
+          end: Alignment.topCenter,
+        ),
+      ),
+      child: Center(
+        child: Text('Hello world!',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 32,
+            )),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:first_app/gradient_container.dart';
 
 void main() {
   runApp(
@@ -8,31 +9,4 @@ void main() {
       ),
     ),
   );
-}
-
-class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
-
-  @override
-  Widget build(context) {
-    return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            Color.fromARGB(255, 175, 0, 58),
-            Colors.black26,
-          ],
-          begin: Alignment.bottomCenter,
-          end: Alignment.topCenter,
-        ),
-      ),
-      child: Center(
-        child: Text('Hello world!',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 32,
-            )),
-      ),
-    );
-  }
 }
