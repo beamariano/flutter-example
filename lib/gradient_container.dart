5526181fc4468b9c5382121c1517f28eacbb77da
Bea M <64287361+beamariano@users.noreply.github.com>
2023-06-13 15:32:29 +0800

    style widget, type and dynamic text

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:first_app/styled_text.dart';
 
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
@@ -16,12 +17,8 @@ class GradientContainer extends StatelessWidget {
           end: Alignment.topCenter,
         ),
       ),
-      child: Center(
-        child: Text('Hello world!',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 32,
-            )),
+      child: const Center(
+        child: StyledText('Kamusta?'),
       ),
     );
   }
